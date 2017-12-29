f36fb443688dcf7178621dc5affb7517ce9033d0
weixiaoyi <2278095567@>
2017-12-29 18:05:46 +0800

    审核页

--- src/consts/urls.js
+++ src/consts/urls.js
@@ -1,7 +1,7 @@
 //URL
 const GET_USER_LIST='/user/getAllUsersInfo'
 const GET_COACH_LIST='/user/getAllCoachesInfo'
-const GET_COACH_INFO='/user/coachinfo'
+const GET_COACH_INFO='/user/coachinfo/:id'
 export default {
   GET_USER_LIST,
   GET_COACH_LIST,
