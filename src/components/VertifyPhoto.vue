e032bba9da32c25fe9759ce67c9c7488faeef07c
weixiaoyi <2278095567@>
2018-01-03 15:56:00 +0800

    点击

--- src/components/VertifyPhoto.vue
+++ src/components/VertifyPhoto.vue
@@ -2,7 +2,9 @@
   <div class="photos">
     <h6>{{label}}</h6>
     <ul class="list">
-      <li v-for="(item,index) in photos" @click="click(index)" :key=""><img :src="getimgAdress(item,index)" /><span>{{item.status}}</span></li>
+      <li v-for="(item,index) in photos" @click="click(index)" :key=""><img :src="getimgAdress(item,index)" />
+        <i class="iconfont icon-tanhao" v-if="!item.status"></i>
+      </li>
     </ul>
   </div>
 </template>
@@ -45,31 +47,36 @@
     width:100%;
     display: flex;
     justify-content:flex-end;
-
-  h6{
-    //border:1px solid red;
-    font-size: .16rem;
-    margin-right: 0.2rem;
-    white-space:nowrap;
-  }
-  ul.list{
-    width:5.1rem !important;
-    //border:1px solid blue;
-    display:flex;
-    flex-wrap:wrap;
-  li{
-    //border:1px solid red;
-    margin:.1rem;
-    margin-top: 0;
-    margin-bottom: .2rem;
-    width: 2.25rem;
-    height:1.5rem;
-    background: #D8D8D8;
-    img{
-      width:100%;
-      height:100%;
+    h6{
+      font-size: .16rem;
+      margin-right: 0.2rem;
+      white-space:nowrap;
     }
-  }
-  }
+    ul.list{
+      width:5.1rem !important;
+      display:flex;
+      flex-wrap:wrap;
+      li{
+        position:relative;
+        margin:.1rem;
+        margin-top: 0;
+        margin-bottom: .2rem;
+        width: 2.25rem;
+        height:1.5rem;
+        background: #D8D8D8;
+        i{
+          font-size: .2rem;
+          color:#FC7563;
+          position:absolute;
+          right:3px;
+          bottom:3px;
+        }
+
+        img{
+          width:100%;
+          height:100%;
+        }
+      }
+    }
   }
 </style>
